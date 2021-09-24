2b75a68848af657d6c0b1e3acdf638c9ec4922d1
ljy <lijiayong@rp-pet.cn>
2021-09-24 19:47:58 +0800

    build: 添加eslint忽略文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    semi: [2, "never"], // 去掉尾部分号
-    "comma-dangle": ["error", "never"], // 去掉最后一个对象的逗号
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    semi: [2, 'never'], // 去掉尾部分号
+    'comma-dangle': ['error', 'never'] // 去掉最后一个对象的逗号
+  }
+}
